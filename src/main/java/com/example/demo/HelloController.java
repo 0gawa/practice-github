0b1a9ac9f1b00bb89fd41a2414f353968ed31b76
0gawa <ogawa.daiki.infratop@gmail.com>
2026-02-16 21:10:08 +0900

    初期段階のcommitの完了

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot 4.0.2 in Docker! (Running on Java 25)";
+    }
+}
