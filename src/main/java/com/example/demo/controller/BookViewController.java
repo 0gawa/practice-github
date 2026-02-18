e619044c8f5726a1c5619d5245cc72d6a8d19e51
0gawa <ogawa.daiki.infratop@gmail.com>
2026-02-18 22:45:27 +0900

    編集機能：controllerの作成

--- src/main/java/com/example/demo/controller/BookViewController.java
+++ src/main/java/com/example/demo/controller/BookViewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,6 +36,24 @@ public class BookViewController {
   @PostMapping("/new")
   public String registerBook(@Valid Book book, BindingResult result) {
     if (result.hasErrors()) {
+      return "book_form";
+    }
+    bookRepository.save(book);
+    return "redirect:/books";
+  }
+
+  @GetMapping("/edit/{id}")
+  public String showEditForm(@PathVariable Long id, Model model) {
+    Book book = bookRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + id));
+    model.addAttribute("book", book);
+    return "book_form";
+  }
+
+  @PostMapping("/update/{id}")
+  public String updateBook(@PathVariable Long id, @Valid Book book, BindingResult result) {
+    if (result.hasErrors()) {
+      book.setId(id);
       return "book_form";
     }
     bookRepository.save(book);
